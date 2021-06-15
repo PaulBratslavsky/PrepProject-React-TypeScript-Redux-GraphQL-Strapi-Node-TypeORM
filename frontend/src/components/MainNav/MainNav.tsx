752814fc6ea07d7497a35f4c9b4f8ea0c21b1453
paul bratslavsky <paulbratslavsky@pauls-MacBook-Pro.local>
2021-06-15 02:18:31 -0500

    refactored nav to its own component

---
+++ frontend/src/components/MainNav/MainNav.tsx
@@ -0,0 +1,17 @@
+import { Nav, Navbar, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../redux/slice/userSlice";
+
+export default function MainNav() {
+  const dispatch = useDispatch();
+
+  return (
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto"></Nav>
+      <Button onClick={() => dispatch(logoutUser())} variant="primary">
+        Logout
+      </Button>
+    </Navbar>
+  );
+}
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { useDispatch } from 'react-redux';
-import { logoutUser } from '../redux/slice/userSlice';
+import { Container, Row, Col } from "react-bootstrap";
+import MainNav from "../components/MainNav/MainNav";
 
-import { Container, Row, Col, Nav, Navbar, Button } from "react-bootstrap";
 export default function Main() {
-  const dispatch = useDispatch();
   return (
     <React.Fragment>
-      <Navbar bg="light" variant="light">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-
-        </Nav>
-        <Button onClick={() => dispatch(logoutUser())} variant="primary">Logout</Button>
-      </Navbar>
+      <MainNav />
       <Container fluid>
       <Row>
         <Col lg={2}>1 of 1</Col>
