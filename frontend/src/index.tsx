31c8485de8ec29a81923c9b0176b53da9ee9015e
paul bratslavsky <paulbratslavsky@pauls-MacBook-Pro.local>
2021-06-15 01:27:42 -0500

    added redux persist to save redux store to local state

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,18 +4,24 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import { client } from './apollo/index'
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import store from "./redux/store";
 import "./sass/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+let persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <Router>
         <App />
       </Router>
+      </PersistGate>
     </Provider>
     </ApolloProvider>
   </React.StrictMode>,
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './slice/userSlice'
+import {combineReducers} from "redux"; 
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage'
+
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistReducer,
+} from "redux-persist";
+
+import userReducer from "./slice/userSlice";
+
+const reducers = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: {
-    user: userReducer
-  }
-})
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
