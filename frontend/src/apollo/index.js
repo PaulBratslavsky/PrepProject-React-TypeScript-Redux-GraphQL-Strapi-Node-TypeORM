a710b510a17b659634ce49c13080a018729f0b76
macmini <macmini@macminis-Mac-mini.local>
2021-06-16 01:41:53 -0500

    cleaning up obseen console logs

--- frontend/src/apollo/index.js
+++ frontend/src/apollo/index.js
@@ -6,7 +6,6 @@ import {
   concat,
 } from "@apollo/client";
 
-// const uri = "http://localhost:1337/graphql";
 const cache = new InMemoryCache();
 const defaultOptions = { mutate: { errorPolicy: "all" } };
 
@@ -27,13 +26,8 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   return forward(operation, "fatter DECk");
 });
 
-console.log(httpLink, "http dexk")
-console.log(authMiddleware, "fuck")
-console.log(concat(authMiddleware, httpLink))
-
 export const client = new ApolloClient({
   cache,
   link: concat(authMiddleware, httpLink),
-  // uri: "http://localhost:1337/graphql",
   defaultOptions,
 });
